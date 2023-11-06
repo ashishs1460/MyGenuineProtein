cd033cd2c9f7e539709a77494a639d016a4733f2
Ashish S <ashish@Ashishs-MacBook-Air.local>
2023-11-06 11:56:25 +0530

    implemented pagination in the user mangement table

--- src/main/java/com/ashish/MyGenuineProtein/controller/AdminUserController.java
+++ src/main/java/com/ashish/MyGenuineProtein/controller/AdminUserController.java
@@ -5,6 +5,9 @@ import com.ashish.MyGenuineProtein.repository.RoleRepository;
 import com.ashish.MyGenuineProtein.repository.UserRepository;
 import com.ashish.MyGenuineProtein.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,11 +28,18 @@ public class AdminUserController {
     RoleRepository roleRepository;
 
     @GetMapping("/admin/getUsers")
-    public String getUsersPage(Model model){
-        model.addAttribute("users",userRepository.findAll());
-        model.addAttribute("role",roleRepository.findAll());
-        return "/getUsers";
+    public String getUsersPage(Model model, @PageableDefault(size = 5) Pageable pageable) {
+        Page<User> usersPage = userRepository.findAll(pageable);
+        model.addAttribute("users", usersPage);
+        model.addAttribute("role", roleRepository.findAll());
 
+        // Calculate total pages and current page
+        int totalPages = usersPage.getTotalPages();
+        int currentPage = usersPage.getNumber();
+
+        model.addAttribute("totalPages", totalPages);
+        model.addAttribute("currentPage", currentPage);
+        return "getUsers";
     }
 
     @GetMapping("/admin/block/{id}")
--- src/main/java/com/ashish/MyGenuineProtein/repository/UserRepository.java
+++ src/main/java/com/ashish/MyGenuineProtein/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.ashish.MyGenuineProtein.repository;
 
 import com.ashish.MyGenuineProtein.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,6 @@ import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findUserByEmail(String email);
+
+    Page<User> findAll(Pageable pageable);
 }
